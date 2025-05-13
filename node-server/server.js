c6093d551c644ab8945a8a3cfe897a3aecafcfa6
HP <samu.kauppi@metropolia.fi>
2025-05-13 13:53:43 +0300

    add login

--- node-server/server.js
+++ node-server/server.js
@@ -9,10 +9,10 @@ app.use(express.urlencoded({ extended: true })); // Parses URL-encoded bodies
 
 // MySQL connection
 const db = mysql.createConnection({
-    host: 'localhost',    // Database host
-    user: 'root',         // Database user
-    password: '',         // Database password
-    database: "awas_db"     // Database name
+    host: process.env.DB_HOST,          // Database host
+    user: process.env.DB_USER,          // Database user
+    password: process.env.DB_PASS,      // Database password
+    database: process.env.DB_NAME       // Database name
 });
 
 db.connect(err => {
@@ -20,7 +20,7 @@ db.connect(err => {
         console.error('DB connection failed:', err.stack);
         return;
     }
-    console.log('Connected to MySQL');
+    console.log('Connected to database');
 });
 
 app.post('/register', (req, res) => {
@@ -29,10 +29,25 @@ app.post('/register', (req, res) => {
     const money = 0;
 
     const sql = `INSERT INTO users (username, password, money) VALUES ('${username}', '${password}', ${money})`;
-
     db.query(sql, (err, results) => {
         if (err) return res.status(500).send(err.message);
         res.send('User registered');
+    });
+});
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const sql = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).send(err.message);
+
+        if (results.length > 0) {
+            return res.send('Login successful');
+        } else {
+            return res.send('Login failed');
+        }
     });
 });
 
